cc2fd0d464e6acd69ad7ad8b9cf4fb6338ab6c7e
Ermanno Goletto <e.goletto@outlook.it>
2017-07-19 18:52:37 +0200

    Update Start-Backup.cmd

--- Backup-GLPI/Start-Backup.cmd
+++ Backup-GLPI/Start-Backup.cmd
@@ -11,10 +11,10 @@ SET MariaDBBackupsRetained=30
 REM *** Impostazioni di configurazione Backup Database GLPI ***
 SET BackupGLPIDatabaseRoot=Z:\Backup-DB-GLPI
 SET GLPIDatabaseName=glpi
-SET GLPIDatabaseName=30
+SET GLPIDatabaseBackupsRetained=30
 
 REM *** Backup MariaDB ***
 powershell -ExecutionPolicy RemoteSigned -File %~dp0\Backup-MariaDB.ps1 "%MariaBackupFile%" %BackupMariaDBRoot% %MariaDBHost% %User% %Password% %MariaDBBackupsRetained%
 
 REM *** Backup Database GLPI ***powershell -ExecutionPolicy RemoteSigned -File %~dp0\Backup-DB-GLPI.ps1 "%MariaBackupFile%" %BackupGLPIDatabaseRoot% %MariaDBHost% %GLPIDatabaseName% %User% %Password% 
-%GLPIDBBackupsRetained%
+%GLPIDatabaseBackupsRetained%
