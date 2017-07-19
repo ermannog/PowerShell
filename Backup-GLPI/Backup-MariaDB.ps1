d180103dfd1a2bf4c56f269433a2c72af0c77300
Ermanno Goletto <e.goletto@outlook.it>
2017-07-19 16:40:40 +0200

    Create Backup-MariaDB.ps1

---
+++ Backup-GLPI/Backup-MariaDB.ps1
@@ -0,0 +1,39 @@
+Param( 
+ [string]$MariaBackupFile, 
+ [string]$BackupRoot ,
+ [string]$User,
+ [string]$Password,
+ [uint32]$BackupsRetained
+)
+
+Set-strictmode -version latest
+
+# Impostazioni di Test
+#$MariaBackupFile= $env:ProgramFiles + "\MariaDB 10.2\bin\mariabackup.exe"
+#$BackupRoot="Z:\Backup-MariaDB"
+#$User="root"
+#$Password="P@assW0rd!"
+#$BackupsRetained = 10
+
+# Inizializzazione Backup
+$BackupPath=$BackupRoot + "\" + (Get-Date -format yyyy-MM-dd)
+If (Test-Path $BackupPath) {Remove-Item $BackupPath -Force -Recurse}
+New-Item -ItemType Directory -Force -Path $BackupPath
+
+# Avvio backup MariaDB
+$TargetArg = "--target-dir=" + $BackupPath
+$UserArg = "--user=" + $User
+$PasswordArg = "--password=" + $password
+
+# Invoke-Expression $BackupMariaDBCommand
+Start-Process -NoNewWindow -FilePath $MariaBackupFile -ArgumentList "--backup", $TargetArg, $UserArg, $PasswordArg, "--host=."
+
+# Eliminazione backup obsoleti
+$BackupDirectories = (Get-ChildItem -Directory $BackupRoot | Sort FullName)
+$BackupsCount = ($BackupDirectories | Measure-Object).Count
+
+If ($BackupsCount -gt $BackupsRetained){
+  For ($i = 1; $i -le $BackupsCount – $BackupsRetained; $i++) {
+    Remove-Item $BackupDirectories[$i-1].FullName -Force -Recurse
+  }
+}
