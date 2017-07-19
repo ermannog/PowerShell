5bbc09e660095e72dc93c002c43b9e13ae097682
Ermanno Goletto <e.goletto@outlook.it>
2017-07-19 18:10:07 +0200

    Update Backup-MariaDB.ps1

--- Backup-GLPI/Backup-MariaDB.ps1
+++ Backup-GLPI/Backup-MariaDB.ps1
@@ -29,7 +29,7 @@ $UserArg = "--user=" + $User
 $PasswordArg = "--password=" + $password
 
 # Invoke-Expression $BackupMariaDBCommand
-Start-Process -NoNewWindow -FilePath $MariaBackupFile -ArgumentList "--backup", $TargetArg, $MariaDBHostArg, $UserArg, $PasswordArg
+Start-Process -NoNewWindow -Wait -FilePath $MariaBackupFile -ArgumentList "--backup", $TargetArg, $MariaDBHostArg, $UserArg, $PasswordArg
 
 # Eliminazione backup obsoleti
 $BackupDirectories = (Get-ChildItem -Directory $BackupRoot | Sort FullName)
