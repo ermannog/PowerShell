78a6192aa4848d0568106dddeb457e014046fd7f
Ermanno Goletto <e.goletto@outlook.it>
2017-07-19 17:32:03 +0200

    Create Backup-DB-GLPI.ps1

---
+++ Backup-GLPI/Backup-DB-GLPI.ps1
@@ -0,0 +1,44 @@
+Param( 
+ [string]$MariaBackupFile, 
+ [string]$BackupRoot,
+ [string]$MariaDBHost,
+ [string]$GLPIDatabaseName,
+ [string]$User,
+ [string]$Password,
+ [uint32]$BackupsRetained
+)
+
+Set-strictmode -version latest
+
+# Impostazioni di Test
+#$MariaBackupFile= $env:ProgramFiles + "\MariaDB 10.2\bin\mariabackup.exe"
+#$BackupRoot="Z:\Backup-MariaDB"
+#$GLPIDatabaseName="glpi"
+#$User="root"
+#$Password="P@assW0rd!"
+#$BackupsRetained = 10
+
+# Inizializzazione Backup
+$BackupPath=$BackupRoot + "\" + (Get-Date -format yyyy-MM-dd)
+If (Test-Path $BackupPath) {Remove-Item $BackupPath -Force -Recurse}
+New-Item -ItemType Directory -Force -Path $BackupPath
+
+# Avvio backup MariaDB
+$TargetArg = "--target-dir=" + $BackupPath
+$MariaDBHostArg = "--host=" + $MariaDBHost
+$GLPIDatabaseNameArg = "--databases=" + $GLPIDatabaseName
+$UserArg = "--user=" + $User
+$PasswordArg = "--password=" + $password
+
+# Invoke-Expression $BackupMariaDBCommand
+Start-Process -NoNewWindow -FilePath $MariaBackupFile -ArgumentList "--backup", $TargetArg, $MariaDBHostArg, $GLPIDatabaseNameArg, $UserArg, $PasswordArg
+
+# Eliminazione backup obsoleti
+$BackupDirectories = (Get-ChildItem -Directory $BackupRoot | Sort FullName)
+$BackupsCount = ($BackupDirectories | Measure-Object).Count
+
+If ($BackupsCount -gt $BackupsRetained){
+  For ($i = 1; $i -le $BackupsCount – $BackupsRetained; $i++) {
+    Remove-Item $BackupDirectories[$i-1].FullName -Force -Recurse
+  }
+}
