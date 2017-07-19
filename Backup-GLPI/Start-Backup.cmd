4739ddf943a29660c22c9caf16646831ad936b0c
Ermanno Goletto <e.goletto@outlook.it>
2017-07-19 19:08:31 +0200

    Update Start-Backup.cmd

--- Backup-GLPI/Start-Backup.cmd
+++ Backup-GLPI/Start-Backup.cmd
@@ -5,16 +5,24 @@ SET User=root
 SET Password=P@assW0rd!
 
 REM *** Impostazioni di configurazione Backup MariaDB ***
-SET BackupMariaDBRoot=Z:\Backup-MariaDB
+SET MariaDBBackupRoot=Z:\Backup-MariaDB
 SET MariaDBBackupsRetained=30
 
 REM *** Impostazioni di configurazione Backup Database GLPI ***
-SET BackupGLPIDatabaseRoot=Z:\Backup-DB-GLPI
+SET GLPIDatabaseBackupRoot=Z:\Backup-DB-GLPI
 SET GLPIDatabaseName=glpi
 SET GLPIDatabaseBackupsRetained=30
 
-REM *** Backup MariaDB ***
-powershell -ExecutionPolicy RemoteSigned -File %~dp0\Backup-MariaDB.ps1 "%MariaBackupFile%" %BackupMariaDBRoot% %MariaDBHost% %User% %Password% %MariaDBBackupsRetained%
+REM *** Impostazioni di configurazione Backup Web Application GLPI ***
+SET GLPIWebApplicationBackupRoot=Z:\Backup-WebApplication-GLPI
+SET GLPIWebApplicationPath=C:\inetpub\wwwroot\glpi
+SET GLPIWebApplicationBackupsRetained=30
 
-REM *** Backup Database GLPI ***powershell -ExecutionPolicy RemoteSigned -File %~dp0\Backup-DB-GLPI.ps1 "%MariaBackupFile%" %BackupGLPIDatabaseRoot% %MariaDBHost% %GLPIDatabaseName% %User% %Password% 
-%GLPIDatabaseBackupsRetained%
+REM *** Backup MariaDB ***
+powershell -ExecutionPolicy RemoteSigned -File %~dp0\Backup-MariaDB.ps1 "%MariaBackupFile%" %MariaDBBackupRoot% %MariaDBHost% %User% %Password% %MariaDBBackupsRetained%
+
+REM *** Backup Database GLPI ***
+powershell -ExecutionPolicy RemoteSigned -File %~dp0\Backup-DB-GLPI.ps1 "%MariaBackupFile%" %GLPIDatabaseBackupRoot% %MariaDBHost% %GLPIDatabaseName% %User% %Password% %GLPIDatabaseBackupsRetained%
+
+REM *** Backup Web Application GLPI ***
+powershell -ExecutionPolicy RemoteSigned -File %~dp0\Backup-WebApplication-GLPI.ps1 %GLPIWebApplicationBackupRoot% %GLPIWebApplicationPath% %GLPIWebApplicationBackupsRetained%
