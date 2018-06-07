0cd79334eef2994d7f5c2b277f376d423feb1e79
Ermanno Goletto <e.goletto@outlook.it>
2018-06-08 00:34:49 +0200

    Delete Set-Wallpaper-EUMETSAT_RGBNatColour_CentralEurope-Center.cmd

--- Set-Wallpaper/Set-Wallpaper-EUMETSAT_RGBNatColour_CentralEurope-Center.cmd
+++
@@ -1,9 +0,0 @@
-REM http://oiswww.eumetsat.org/IPPS/html/latestImages.html
-
-SET URI='http://oiswww.eumetsat.org/IPPS/html/latestImages/EUMETSAT_MSG_RGBNatColour_CentralEurope.jpg'
-SET FILE='%~dp0Wallpaper.jpg'
-
-Powershell Invoke-WebRequest -Uri %URI% -UseDefaultCredentials -Proxy ([System.Net.WebRequest]::GetSystemWebproxy()).GetProxy(%URI%) -ProxyUseDefaultCredentials -OutFile %FILE%
-Powershell -ExecutionPolicy RemoteSigned -Command %~dp0Set-Wallpaper.ps1 %FILE%
-
-PAUSE
