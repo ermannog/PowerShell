16c8b8a40d61579100e54e8009c0d94706ced1c9
Ermanno Goletto <e.goletto@outlook.it>
2018-11-29 18:39:26 +0100

    Update Test-WebProxy-CurrentUser.cmd

--- Test-Proxy/Test-WebProxy-CurrentUser.cmd
+++ Test-Proxy/Test-WebProxy-CurrentUser.cmd
@@ -1,2 +1,2 @@
-@powershell -ExecutionPolicy RemoteSigned -Command %~dp0Test-WebProxy.ps1 -Url https://www8.hp.com/it/it/home.html -ProxyHost sfftmg00.comune.cuneo.it -ProxyPort 8080
+@powershell -ExecutionPolicy RemoteSigned -Command %~dp0Test-WebProxy.ps1 -Url https://www.microsoft.com -ProxyHost proxy.contoso.com -ProxyPort 8080
 @PAUSE
