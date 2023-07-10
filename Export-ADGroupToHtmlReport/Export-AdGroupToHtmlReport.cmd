fd576c4079df7cbc5a7d352eaab6536e4d4fd53c
Ermanno Goletto <e.goletto@outlook.it>
2023-07-10 10:01:45 +0200

    Add files via upload

---
+++ Export-ADGroupToHtmlReport/Export-AdGroupToHtmlReport.cmd
@@ -0,0 +1,11 @@
+@ECHO OFF
+CLS
+REM SET GroupID=Tutti i Dipendenti
+SET GroupID=Utenti-Prova
+SET /p "GroupID=Nome gruppo (Invio <%GroupID%>): "
+SET Note=Modifica gruppo
+SET /p "Note=Annotazioni (Invio <%Note%>): "
+
+powershell -ExecutionPolicy RemoteSigned -Command %~dp0Export-AdGroupToHtmlReport.ps1 -GroupId '%GroupID%' -Note '%Note%' -OpenReport
+
+IF %errorlevel% NEQ 0 PAUSE
