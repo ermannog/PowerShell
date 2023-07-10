9ea75804736609604f7c541a5e68364b1d950910
Ermanno Goletto <e.goletto@outlook.it>
2023-07-10 10:04:46 +0200

    Update and rename Export-AdGroupToHtmlReport-Utenti_e_PC_Comunali.cmd to Export-AdGroupToHtmlReport-OUSales.cmd

---
+++ Export-ADGroupToHtmlReport/Export-AdGroupToHtmlReport-OUSales.cmd
@@ -0,0 +1,10 @@
+@ECHO OFF
+CLS
+
+SET OUDN="OU=Sales,DC=contoso,DC=com"
+
+FOR /f "delims=*" %%i in ('dsquery group %OUDN% -limit 0 -o samid') DO (
+  powershell -ExecutionPolicy RemoteSigned -File %~dp0Export-AdGroupToHtmlReport.ps1 -GroupId %%~i -Note "Generazione massiva" -PathDirectoryReports "%~dp0Reports\Sales"
+)
+
+Pause
