c0185c84df2a3e207308282552a69eeb3bfc433a
Ermanno Goletto <e.goletto@outlook.it>
2023-09-06 15:35:22 +0200

    Update Export-AdGroupToHtmlReport-OUSales.cmd

--- Export-ADGroupToHtmlReport/Export-AdGroupToHtmlReport-OUSales.cmd
+++ Export-ADGroupToHtmlReport/Export-AdGroupToHtmlReport-OUSales.cmd
@@ -4,7 +4,8 @@ CLS
 SET OUDN="OU=Sales,DC=contoso,DC=com"
 
 FOR /f "delims=*" %%i in ('dsquery group %OUDN% -limit 0 -o samid') DO (
-  powershell -ExecutionPolicy RemoteSigned -File %~dp0Export-AdGroupToHtmlReport.ps1 -GroupId %%~i -Note "Generazione massiva" -PathDirectoryReports "%~dp0Reports\Sales"
+  powershell -ExecutionPolicy RemoteSigned -File %~dp0Export-AdGroupToHtmlReport.ps1 -GroupId "%%~i" -Note "Generazione massiva" -PathDirectoryReports "%~dp0Reports\Sales"
+  IF %errorlevel% NEQ 0 PAUSE
 )
 
 Pause
