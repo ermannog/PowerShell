238a0aa73775fb8dae83e1a7ce75538ca13c3cca
Ermanno Goletto <e.goletto@outlook.it>
2023-09-06 15:57:58 +0200

    Update Export-AdUserToHtmlReport-OUSales.cmd

--- Export-ADUserToHtmlReport/Export-AdUserToHtmlReport-OUSales.cmd
+++ Export-ADUserToHtmlReport/Export-AdUserToHtmlReport-OUSales.cmd
@@ -4,7 +4,8 @@ CLS
 SET OUDN="OU=Sales,DC=contoso,DC=com"
 
 FOR /f "delims=*" %%i in ('dsquery user %OUDN% -limit 0 -o samid') DO (
-  powershell -ExecutionPolicy RemoteSigned -File %~dp0Export-AdUserToHtmlReport.ps1 -UserId %%~i -Note "Generazione massiva" -PathDirectoryReports "%~dp0Reports\Sales"
+  powershell -ExecutionPolicy RemoteSigned -File %~dp0Export-AdUserToHtmlReport.ps1 -UserId "%%~i" -Note "Generazione massiva" -PathDirectoryReports "%~dp0Reports\Sales"
+  IF %errorlevel% NEQ 0 PAUSE
 )
 
 Pause
