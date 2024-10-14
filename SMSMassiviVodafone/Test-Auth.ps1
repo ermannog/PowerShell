ef948fd2e543c9ccfead4d824bc31c7d1f7d23dd
Ermanno Goletto <e.goletto@outlook.it>
2024-10-14 15:12:52 +0200

    Add files via upload

---
+++ SMSMassiviVodafone/Test-Auth.ps1
@@ -0,0 +1,22 @@
+$registrationEmail = "registration email address"
+$apiPassword ="API Password" 
+
+# Authenticate using a user token
+$url = "https://smsmassivi.vodafone.it/API/v1.0/REST/token"
+
+$authHeader = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes("${registrationEmail}:$apiPassword"))
+
+$headers = @{
+    "Content-Type" = "application/json"
+    "Authorization" = "Basic $authHeader"
+}
+
+
+$response = Invoke-RestMethod -Uri $url -Method Get -Headers $headers
+
+# Authenticate Response
+$userKey = ($response -split ';')[0]
+$accessToken = ($response -split ';')[1]
+
+Write-Host "USER_KEY: $userKey"
+Write-Host "ACCESS_TOKEN: $accessToken"
